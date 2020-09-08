35f3c0f862218c5e675a10688885a1b1f23ce45b
Fabrice DUJARDIN <44850811+fabricedujardinportfolio@users.noreply.github.com>
2020-09-08 15:49:58 +0200

    rajout de contenue js

---
+++ site/cci/text-scramble-effect/README.markdown
@@ -0,0 +1,5 @@
+# Text Scramble Effect
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/soulwire/pen/mErPAK](https://codepen.io/soulwire/pen/mErPAK).
+
+A little text decoding / scramble effect
--- site/cci/text-scramble-effect/dist/script.js
+++ site/cci/text-scramble-effect/dist/script.js
@@ -1,65 +1,3 @@
-// https://webkit.org/blog-files/3d-transforms/morphing-cubes.html
-
-function hasClassName(inElement, inClassName)
-{
-    var regExp = new RegExp('(?:^|\\s+)' + inClassName + '(?:\\s+|$)');
-    return regExp.test(inElement.className);
-}
-
-function addClassName(inElement, inClassName)
-{
-    if (!hasClassName(inElement, inClassName))
-        inElement.className = [inElement.className, inClassName].join(' ');
-}
-
-function removeClassName(inElement, inClassName)
-{
-    if (hasClassName(inElement, inClassName)) {
-        var regExp = new RegExp('(?:^|\\s+)' + inClassName + '(?:\\s+|$)', 'g');
-        var curClasses = inElement.className;
-        inElement.className = curClasses.replace(regExp, ' ');
-    }
-}
-
-function toggleClassName(inElement, inClassName)
-{
-    if (hasClassName(inElement, inClassName))
-        removeClassName(inElement, inClassName);
-    else
-        addClassName(inElement, inClassName);
-}
-
-function toggleShape()
-{
-  var shape = document.getElementById('shape');
-  if (hasClassName(shape, 'ring')) {
-    removeClassName(shape, 'ring');
-    addClassName(shape, 'cube');
-  } else {
-    removeClassName(shape, 'cube');
-    addClassName(shape, 'ring');
-  }
-
-  // Move the ring back in Z so it's not so in-your-face.
-  var stage = document.getElementById('stage');
-  if (hasClassName(shape, 'ring'))
-    stage.style.webkitTransform = 'translateZ(-200px)';
-  else
-    stage.style.webkitTransform = '';
-}
-
-function toggleBackfaces()
-{
-  var backfacesVisible = document.getElementById('backfaces').checked;
-  var shape = document.getElementById('shape');
-  if (backfacesVisible)
-    addClassName(shape, 'backfaces');
-  else
-    removeClassName(shape, 'backfaces');
-}
-
-
-
 // ——————————————————————————————————————————————————
 // TextScramble
 // ——————————————————————————————————————————————————
@@ -123,13 +61,13 @@ class TextScramble {
 // ——————————————————————————————————————————————————
 
 const phrases = [
-'CCI',
-'CLASS DEV 2020',
-'Vas vous faire réaliser',
-'Qu\'il y a une différence',
-'Entre connaître le chemin',
-'Et marcher sur le chemin',
-'L\'avenir de demain nous vous le proposerons !'];
+'Neo,',
+'sooner or later',
+'you\'re going to realize',
+'just as I did',
+'that there\'s a difference',
+'between knowing the path',
+'and walking the path'];
 
 
 const el = document.querySelector('.text');
@@ -138,7 +76,7 @@ const fx = new TextScramble(el);
 let counter = 0;
 const next = () => {
   fx.setText(phrases[counter]).then(() => {
-    setTimeout(next, 1000);
+    setTimeout(next, 800);
   });
   counter = (counter + 1) % phrases.length;
 };
